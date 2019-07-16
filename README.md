1cface42ebb03b1dd4795a384c2a504128f0ab77
hackett <23925493@qq.com>
2019-07-16 20:02:19 +0800

    .

--- README.md
+++ README.md
@@ -1,14 +1,86 @@
 # frame
 
-A new Flutter package project.
+A set of utilities that allow you to easily pass a data Model from a parent Widget down to it's descendants.
 
-## Getting Started
+This Library provides two main classes:
 
-This project is a starting point for a Dart
-[package](https://flutter.dev/developing-packages/),
-a library module containing code that can be shared easily across
-multiple Flutter or Dart projects.
+  * The `Provider` Widget. If you need to pass a `Store` deep down your Widget hierarchy, you can wrap your `Store` in a `Provider` Widget. This will make the Model available to all descendant Widgets.
+  * The `StateWidget` Widget. Use this Widget to find the appropriate `Store` in the Widget tree. It will automatically rebuild whenever the Model notifies that change has taken place.
 
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.dev/docs), which offers tutorials, 
-samples, guidance on mobile development, and a full API reference.
+## Examples
+
+  * [Counter App](https://github.com/hackett0/flutter_frame/master/example) - Introduction to the tools provided by frame. 
+
+## Usage
+
+Let's demo the basic usage with the all-time favorite: A counter example!
+
+```dart
+class Counter extends ChangeNotifier {
+  int value = 0;
+
+  void increment() {
+    value += 1;
+    notifyListeners();
+  }
+}
+
+class Store {
+  final counter = Counter();
+}
+
+main() => runApp(Provider(
+      store: Store(),
+      child: MyApp(),
+    ));
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Frame Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Frame Demo Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('You have pushed the button this many times:'),
+            StateWidget(
+              state: (store) => store.counter,
+              builder: (context, child, counter) => Text(
+                '${counter.value.toString()}',
+                style: Theme.of(context).textTheme.display1,
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () =>
+            StateWidget.of(context, (store) => store.counter).increment(),
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+```
+
+## Contributors
+
+  * [Hackett](https://github.com/hackett0)
--- example/example.dart
+++ example/example.dart
@@ -1,44 +1,6 @@
-/*
- * @Author: Hackett 
- * @Date: 2019-07-09 19:18:36 
- * @Last Modified by: Hackett
- * @Last Modified time: 2019-07-09 19:29:51
- */
-
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:frame/frame.dart';
 
-void main() {
-  // Initialize the model. Can be done outside a widget, like here.
-  var counter = Counter();
-
-  // Here we set up a delayed interaction with the model (increment each
-  // 5 seconds), outside of the Flutter widget tree.
-  //
-  // This is just an example. In a real world app, this could be replaced
-  // with a connection to a real-time database, for example.
-  Timer.periodic(
-    const Duration(seconds: 5),
-    (timer) => counter.increment(),
-  );
-
-  // Set up a Providers instance.
-  var providers = Providers();
-  providers.provide(Provider<Counter>.value(counter));
-
-  // Now we're ready to run the app...
-  runApp(
-    // ... and provide the model to all widgets within.
-    ProviderNode(
-      providers: providers,
-      child: MyApp(),
-    ),
-  );
-}
-
-/// Simplest possible model, with just one field.
 class Counter extends ChangeNotifier {
   int value = 0;
 
@@ -48,20 +10,20 @@ class Counter extends ChangeNotifier {
   }
 }
 
-class UserData extends ChangeNotifier {
-  final int id;
-  String name = "";
-
-  UserData(this.id) {
-    Future.delayed(const Duration(seconds: 10), () => name = "Hackett_" + this.id.toString());
-  }
+class Store {
+  final counter = Counter();
 }
+
+main() => runApp(Provider(
+      store: Store(),
+      child: MyApp(),
+    ));
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Frame Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -75,33 +37,26 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter Demo Home Page'),
+        title: Text('Frame Demo Home Page'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text('You have pushed the button this many times:'),
-            // Provide looks for an ancestor ProviderNode widget
-            // and retrieves its model (Counter, in this case).
-            // Then it uses that model to build widgets, and will trigger
-            // rebuilds if the model is updated.
-            Provide<Counter>(
+            StateWidget(
+              state: (store) => store.counter,
               builder: (context, child, counter) => Text(
-                    '${counter.value}',
-                    style: Theme.of(context).textTheme.display1,
-                  ),
+                '${counter.value.toString()}',
+                style: Theme.of(context).textTheme.display1,
+              ),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        // Provide.value is another way to access the model object held
-        // by an ancestor ProviderNode. By default, it just returns
-        // the current model and doesn't automatically trigger rebuilds.
-        // Since this button always looks the same, though, no rebuilds
-        // are needed.
-        onPressed: () => Provide.value<Counter>(context).increment(),
+        onPressed: () =>
+            StateWidget.of(context, (store) => store.counter).increment(),
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ),
